3b6c000170c2df65bfcb07a3a1a86b2d8cfc8300
Юрий Данильченко <genitra111@gmail.com>
2024-08-11 22:20:53 +1000

    Изменения к заданию №2

--- src/core.py
+++ src/core.py
@@ -9,12 +9,40 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecturer(self, lecturer: object, course: str, grade: int):
+        """Выставление оценки лектору"""
+        if (isinstance(lecturer, Lecturer)
+            and course in lecturer.courses_attached
+            and self.courses_in_progress):
+
+            if 11 > grade > 0:
+                if course in lecturer.grades:
+                    lecturer.grades[course] += [grade]
+                else:
+                    lecturer.grades[course] = [grade]
+            else:
+                return 'Недопустимая оценка! Используйте цифры от 1 до 10'
+        else:
+            return 'Ошибка'
+
+
 class Mentor:
     """Класс преподователя"""
     def __init__(self, name: str, surname: str) -> None:
         self.name = name
         self.surname = surname
         self.courses_attached = []
+
+
+class Lecturer(Mentor):
+    """Класс лекторов"""
+    def __init__(self, name: str, surname: str) -> None:
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+class Reviewer(Mentor):
+    """Класс проверяющих экспертов"""
 
     def rate_hw(self, student: object, course: str, grade: int):
         """Выставление студенту оценки за прохождение курса"""
@@ -32,9 +60,3 @@ class Mentor:
                 return 'Недопустимая оценка! Используйте цифры от 1 до 10.'
         else:
             return 'Ошибка'
-
-class Lecturer(Mentor):
-    """Класс лекторов"""
-
-class Reviewer(Mentor):
-    """Класс проверяющих экспертов"""
